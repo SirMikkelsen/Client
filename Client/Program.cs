67f3cc21ad82672f0371734c50b93056761ead48
Gordon Mikkelsen <gordonmikkelsen87@gmail.com>
2018-02-12 16:11:10 +0100

    Færdig med client-side

--- Client/Program.cs
+++ Client/Program.cs
@@ -10,6 +10,20 @@ namespace Client
     {
         static void Main(string[] args)
         {
+            using (var client = new ServiceReference1.Service1Client())
+            {
+               var s1 = client.AddStudent("Bruce wayne", "13b", 7);
+                
+
+                Console.WriteLine(s1.Name);
+
+                var s2 = client.FindStudent(s1.Name);
+
+                Console.WriteLine(s2.Name);
+
+            }
+
+            Console.ReadKey();
         }
     }
 }
